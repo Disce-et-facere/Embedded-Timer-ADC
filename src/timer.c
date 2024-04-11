c9914085d5d472789b8f4486fcf89a40d372729b
Robert Krantz <krantzr@hotmail.se>
2024-04-11 08:01:59 +0200

    Update timer.c

--- src/timer.c
+++ src/timer.c
@@ -4,11 +4,23 @@
 #define TIMER1_PRESCALER 1
 #define TIMER1_TOP_VALUE ((F_CPU / (TIMER1_PRESCALER * 10)) - 1)
 
+
+ISR(TIMER1_COMPA_vect) {
+setTimerOverflow();
+    timer1_overflow_count++;
+}
+
 void initTimer1(){ // timer set to interupt on 1ms.
     TCCR1B |= (1 << WGM12);
     TCCR1B |= (1 << CS10);
     OCR1A = TIMER1_TOP_VALUE;
     TIMSK1 |= (1 << OCIE1A);
+}
+
+void setTimerOverflow(){
+
+static timer1_overflow_count = 0;
+
 }
 
 void millis(uint16_t ms){
